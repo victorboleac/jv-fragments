eaf6c81b910b09b470b6399863438ea604e6e2f7
Victor Boleac <victor.boleac@icloud.com>
2025-01-06 11:51:55 +0000

    feat:Implemented NavigationBarView Interface

--- app/src/main/java/com/example/jv_fragments/MainActivity.java
+++ app/src/main/java/com/example/jv_fragments/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.jv_fragments;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    NavigationBarView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    AddFragment addFragment = new AddFragment();
+    FavouritesFragment favouritesFragment = new FavouritesFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.home){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, homeFragment)
+                    .commit();
+            return true;
+        }
+        if(item.getItemId()==R.id.add){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, addFragment)
+                    .commit();
+            return true;
+        }
+        if(item.getItemId()==R.id.favourites){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, favouritesFragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
